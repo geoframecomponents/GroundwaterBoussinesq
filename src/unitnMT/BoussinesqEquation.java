e680886307f24e8f85de3bdfa2a4eda72b05be6a
francescoS <francesco.serafin.3@gmail.com>
2014-02-01 20:38:14 +0100

    code update

--- src/unitnMT/BoussinesqEquation.java
+++ src/unitnMT/BoussinesqEquation.java
@@ -1,17 +1,20 @@
 package unitnMT;
 
 import cern.colt.matrix.tdouble.DoubleMatrix1D;
+import cern.colt.matrix.tdouble.algo.solver.IterativeSolverDoubleNotConvergedException;
 import cern.colt.matrix.tdouble.impl.SparseDoubleMatrix1D;
 
 public class BoussinesqEquation {
 	
 	double[] arrb;
 	double[] matT;
+	SparseDoubleMatrix1D arrR;
 	int deltat = 1;
 	
 	BoussinesqEquation(int Np, int SIZE){
 		
 		arrb = new double[Np];
+		arrR = new SparseDoubleMatrix1D(Np);
 		matT = new double[SIZE];
 		
 	}
@@ -25,7 +28,7 @@ public class BoussinesqEquation {
 			arrb[i] = (grid1.eta[i]-grid1.bottomElevation[i])*grid1.planArea[i]+deltat*grid1.planArea[i]*grid1.sourceSink[i];
 			for (int j = grid1.Mp[i]; j < grid1.Mp[i+1]; j++){
 				if (grid1.Ml[j] != -1){
-					matT[j] = -deltat*(1/grid1.euclideanDistance[grid1.Ml[j]])*grid1.hydrConductivity[grid1.Ml[j]]*grid1.lengthSides[grid1.Ml[j]]*Math.max(grid1.eta[grid1.Mi[j]+1]-grid1.bottomElevation[grid1.Mi[j]+1], grid1.eta[i+1]-grid1.bottomElevation[i+1]);
+					matT[j] = -deltat*(1/grid1.euclideanDistance[(int) grid1.Ml[j]])*grid1.hydrConductivity[(int) grid1.Ml[j]]*grid1.lengthSides[(int) grid1.Ml[j]]*Math.max(grid1.eta[grid1.Mi[j]+1]-grid1.bottomElevation[grid1.Mi[j]+1], grid1.eta[i+1]-grid1.bottomElevation[i+1]);
 					colSum += -matT[j];
 				} else {index = j;}
 			}
@@ -34,11 +37,32 @@ public class BoussinesqEquation {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public void estimateR(Grid grid1){
+		//calculate R with RC product. ?? values of first attempt
+	}
+	
+	public void estimateP(){
+		//calculate P
+	}
+	
+	public void newtonBEq(Grid grid1) throws IterativeSolverDoubleNotConvergedException {
+		
+		estimateT(grid1);
+		estimateR(grid1);
+		//estimate P
+		//while with R
+		RCConjugateGradient cg = new RCConjugateGradient(grid1.numberSidesPolygon.length,grid1.Mp,grid1.Mi,grid1.Ml);
+		cg.solverCG(arrR);
+		
+		
+		
+	}
+	
+	public static void main(String[] args) throws IterativeSolverDoubleNotConvergedException {
 		
 		Grid grid1 = new Grid();
 		BoussinesqEquation beq = new BoussinesqEquation(grid1.lengthSides.length, grid1.Ml.length);
-		beq.estimateT(grid1);
+		beq.newtonBEq(grid1);
 		
 		
 
--- src/unitnMT/RCConjugateGradient.java
+++ src/unitnMT/RCConjugateGradient.java
@@ -3,17 +3,21 @@ package unitnMT;
 import cern.colt.matrix.tdouble.DoubleMatrix1D;
 import cern.colt.matrix.tdouble.algo.solver.DoubleCG;
 import cern.colt.matrix.tdouble.algo.solver.IterativeSolverDoubleNotConvergedException;
+import cern.colt.matrix.tdouble.impl.SparseDoubleMatrix1D;
 import cern.colt.matrix.tdouble.impl.SparseRCDoubleMatrix2D;
 
 public class RCConjugateGradient {
 	
 	SparseRCDoubleMatrix2D matrix_A;
-	DoubleMatrix1D matrix_b;
 	DoubleMatrix1D matrix_x;
 	
-	RCConjugateGradient(int SIZE, int[] Mp, int[] Mi, double[] Ml, DoubleMatrix1D matrix_b) throws IterativeSolverDoubleNotConvergedException{
+	RCConjugateGradient (int SIZE, int[] Mp, int[] Mi, double[] Ml) {
 		
 		matrix_A = new SparseRCDoubleMatrix2D(SIZE,SIZE,Mp,Mi,Ml);
+		
+	}
+	
+	public void solverCG(SparseDoubleMatrix1D matrix_b) throws IterativeSolverDoubleNotConvergedException {
 		
 		DoubleCG conjugateGradient = new DoubleCG(matrix_x);
 		DoubleMatrix1D matSol = conjugateGradient.solve(matrix_A,
