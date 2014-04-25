d3619b9ed9c5f92a22270983f3dea66d053f18bf
francescoS <francesco.serafin.3@gmail.com>
2014-04-25 09:51:37 +0200

    All the modifies to write the solutions are copied to ComputeBEqDirichlet

--- oopBoussinesqEquation/src/org/boussinesq/boussinesq/dirichletBoundaryConditions/ComputeBEqDirichlet.java
+++ oopBoussinesqEquation/src/org/boussinesq/boussinesq/dirichletBoundaryConditions/ComputeBEqDirichlet.java
@@ -3,6 +3,7 @@ package org.boussinesq.boussinesq.dirichletBoundaryConditions;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 
 import org.boussinesq.RowCompressedForm.RCConjugateGradient;
 //import org.boussinesq.RowCompressedForm.RCConjugateGradient;
@@ -12,6 +13,7 @@ import org.boussinesq.boussinesq.PolygonGeometricalWetProperties;
 import org.boussinesq.boussinesq.TimeSimulation;
 import org.boussinesq.boussinesq.computationalDoman.ComputationalDomain;
 import org.boussinesq.machineEpsilon.MachineEpsilon;
+import org.francescoS.usefulClasses.FileWrite;
 import org.francescoS.usefulClasses.TextIO;
 
 import cern.colt.matrix.tdouble.algo.solver.IterativeSolverDoubleNotConvergedException;
@@ -20,6 +22,8 @@ public class ComputeBEqDirichlet extends ComputeT implements TimeSimulation {
 
 	// allocate the memory for eta array
 	double[] eta;
+	double[] aquiferThickness;
+	double[] volumeSource;
 
 	double[] matT;
 	double[] matTDirichlet;
@@ -43,6 +47,56 @@ public class ComputeBEqDirichlet extends ComputeT implements TimeSimulation {
 		super();
 
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public DecimalFormat computePattern(){
+		
+		int[] c = new int[String.valueOf(SIMULATIONTIME).length()];
+		
+		StringBuilder builder = new StringBuilder(c.length);
+		
+		for (int i:c){
+			
+			builder.append(c[i]);
+			
+		}
+		
+		String pattern = builder.toString();
+		
+		return new DecimalFormat(pattern);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void writeSolution(int time) throws IOException{
+		
+		FileWrite.writeStringIntString("Iteration number", (int) time/TIMESTEP + 1, " [ ]");
+		FileWrite.writeStringDoubleString("Timestep", TIMESTEP, " [s]");
+		FileWrite.writeStringDoubleString("Time of simulation", SIMULATIONTIME, " [s]");
+		FileWrite.writeStringDoubleString("Time of simulation", SIMULATIONTIME/60, " [min]");
+		FileWrite.writeStringDoubleString("Initial volume", volumeOld, "[ m^3]");
+		FileWrite.writeStringDoubleString("Total volume", volumeNew, " [m^3]");
+		FileWrite.writeFourStringColumn("PiezHead","AquifThick","WaterVolume","Source");
+		FileWrite.writeFourStringColumn("[m]", "[m]", "[m^3]", "[m^3/s]");
+		FileWrite.writeFourDoubleColumn(eta,aquiferThickness,volume,volumeSource);
+		FileWrite.closeTxtFile();
+		
+	}
+	
 	
 	
 	
@@ -121,7 +175,12 @@ public class ComputeBEqDirichlet extends ComputeT implements TimeSimulation {
 		MachineEpsilon cMEd = new MachineEpsilon();
 		double tolerance = cMEd.computeMachineEpsilonDouble();
 
+		String fileName = null;
+		DecimalFormat myformatter = computePattern();
+		
 		// allocate the memory for eta array
+		aquiferThickness = new double[ComputationalDomain.Np];
+		volumeSource = new double[ComputationalDomain.Np];
 		eta = new double[ComputationalDomain.Np];
 		volume = new double[ComputationalDomain.Np];
 		
@@ -132,12 +191,13 @@ public class ComputeBEqDirichlet extends ComputeT implements TimeSimulation {
 			
 		}
 
-		FileWriter Rstatfile = new FileWriter(ComputationalDomain.outputPathBeqDirichlet);
-		PrintWriter errestat = new PrintWriter(Rstatfile);
+		TextIO.putln("Initial volume: " + volumeOld);
 
 		System.arraycopy(ComputationalDomain.eta, 0, eta, 0, ComputationalDomain.eta.length);
 
 		for (int t = 0; t < SIMULATIONTIME; t += TIMESTEP) {
+			
+			TextIO.putln("Time step " + (double) t/3600);
 
 			eta = etaInit.etaInitialization(eta);
 
@@ -146,29 +206,29 @@ public class ComputeBEqDirichlet extends ComputeT implements TimeSimulation {
 			eta = newton.newtonIteration(arrb, matTNoDirichlet, indexDiag, eta,
 					cg, tolerance);
 
-			System.out.println("Simulation time: " + (double) t / 3600);
-
 			for (int j = 0; j < eta.length; j++) {
+				
+				volumeSource[j] = ComputationalDomain.source[j] * ComputationalDomain.planArea[j];
+				aquiferThickness[j] = Math.max(0, eta[j]-ComputationalDomain.bedRockElevation[j]);
 				
 				volume[j] = computeVolume(j,eta[j]);
 				volumeNew = volumeNew + volume[j];
 
 			}
 			
-			TextIO.putln("Volume " + volumeNew + "at time step " + t/3600);
+			writeSolution(t);
+			
+			if (Math.abs(volumeNew-volumeOld) > Math.pow(10, -6)){
+				
+				TextIO.putln("WARNING!!! The system is losing mass");
+				TextIO.putln("The difference between initial volume and compute volume is: " + Math.abs(volumeNew-volumeOld));
+				
+			}
 			
 			volumeNew = 0;
 			
 		}
-		for (int j = 0; j < eta.length; j++) {
 
-			errestat.println(eta[j]);
-
-		}
-
-		errestat.println();
-		System.out.println();
-		Rstatfile.close();
 
 		System.out.println("Exit code");
 	}
